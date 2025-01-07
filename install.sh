94bc7c1c88d3a30f9ddae48297f8041fb510dcfb
Thaynaroth <chuekthaynaroth@gmail.com>
2025-01-07 19:26:55 +0700

    install working phoenix 1.17.8 on debain 12 bookworm all in one go

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# update apt
+apt update && upgrade -y
+
+# install dependencies need for building erlang
+apt -y install build-essential autoconf m4 libncurses-dev \
+libwxgtk3.2-dev libwxgtk-webview3.2-dev libgl1-mesa-dev libglu1-mesa-dev libpng-dev libssh-dev unixodbc-dev \
+xsltproc fop libxml2-utils openjdk-17-jdk wget unzip
+
+# build erlang from source version 27
+git clone https://github.com/erlang/otp.git ~/otp-source
+cd ~/otp-source
+git checkout maint-27
+./configure
+make
+make install
+
+# install elixir 1.18.1
+wget https://github.com/elixir-lang/elixir/releases/download/v1.18.1/elixir-otp-27.zip
+unzip elixir-otp-27.zip -d $HOME/.elixir
+rm -f elixir-otp-27.zip
+echo 'export PATH="$PATH:$HOME/.elixir/bin"' >> ~/.bashrc
+source ~/.bashrc
+
+# install phoenix
+mix local.hex --force
+mix local.rebar --force
+mix archive.install hex phx_new --force
